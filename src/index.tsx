4809ce1871188a27a64625a5808bff05063eac22
Alapan Mukherjee <alapan.mukherjee@nightingalehealth.com>
2020-09-23 02:57:31 +0300

    Rename IssueEvents to IssueEventsTable

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { App } from './components/App';
-import { IssueEvents } from './components/IssueEvents';
+import { IssueEventsTable } from './components/IssueEventsTable';
 import { StateProvider } from './state';
 
 ReactDOM.render(
@@ -12,7 +12,7 @@ ReactDOM.render(
                 <Route exact={true} path="/" component={App} />
                 <Route
                     path="/issues/:owner/:repo/:issue_id/events"
-                    component={IssueEvents}
+                    component={IssueEventsTable}
                 />
             </Switch>
         </Router>
